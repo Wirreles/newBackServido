b7d5a4b1022c74db2513235e057b1bfc3e3db982
joelch97 <125751366+Wirreles@users.noreply.github.com>
2025-07-07 16:59:05 -0300

    averga

--- src/firebase.js
+++ src/firebase.js
@@ -2,19 +2,19 @@ require("dotenv").config();
 const { initializeApp, applicationDefault, credential } = require("firebase-admin/app");
 const admin = require("firebase-admin")
 const { getFirestore } = require("firebase-admin/firestore");
-const { readFileSync } = require('fs'); 
+// const { readFileSync } = require('fs'); 
 // LOCALHOST
-// const googleCredentials = require('../app-servicios-e99.json');  
-// initializeApp({
-//   credential: admin.credential.cert(googleCredentials),
-// });
+const googleCredentials = require('../app-servicios-e99.json');  
+initializeApp({
+  credential: admin.credential.cert(googleCredentials),
+});
 
 // ARCHIVO SECRETO RENDER
-const serviceAccount = JSON.parse(readFileSync('/etc/secrets/app-servicios-e99.json', 'utf-8'));  
+// const serviceAccount = JSON.parse(readFileSync('/etc/secrets/app-servicios-e99.json', 'utf-8'));  
 // Inicializar Firebase Admin SDK 
-initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
+// initializeApp({
+//   credential: admin.credential.cert(serviceAccount)
+// });
  
 
 
