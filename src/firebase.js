abfb068b1759687dbde50d36efe93a4959bf947a
joelch97 <125751366+Wirreles@users.noreply.github.com>
2025-07-13 20:53:34 -0300

    .json nuevo

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ const admin = require("firebase-admin")
 const { getFirestore } = require("firebase-admin/firestore");
 // const { readFileSync } = require('fs'); 
 // LOCALHOST
-const googleCredentials = require('../app-servicios-e99.json');  
+const googleCredentials = require('../servidodb2.json');  
 initializeApp({
   credential: admin.credential.cert(googleCredentials),
 });
 
 // ARCHIVO SECRETO RENDER
-// const serviceAccount = JSON.parse(readFileSync('/etc/secrets/app-servicios-e99.json', 'utf-8'));  
+// const serviceAccount = JSON.parse(readFileSync('/etc/secrets/servidodb2.json', 'utf-8'));  
 // Inicializar Firebase Admin SDK 
 // initializeApp({
 //   credential: admin.credential.cert(serviceAccount)
